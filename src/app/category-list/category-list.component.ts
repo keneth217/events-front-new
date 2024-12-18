20e0df3f4113aa0bdad172cd47ce4454941a1ae9
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-18 20:45:12 +0300

    add content to pages,integrate login api

--- src/app/category-list/category-list.component.ts
+++ src/app/category-list/category-list.component.ts
@@ -8,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrl: './category-list.component.css'
 })
 export class CategoryListComponent {
-
+// Array of event objects
+  events = [
+    {
+      title: 'Event Title 1',
+      description: 'Brief details about the event go here.',
+      image: '/assets/3.jpg'
+    },
+    {
+      title: 'Event Title 2',
+      description: 'Brief details about the event go here.',
+      image: '/assets/2.jpg'
+    },
+    {
+      title: 'Event Title 3',
+      description: 'Brief details about the event go here.',
+      image: '/assets/events.jpg'
+    },
+    {
+      title: 'Event Title 4',
+      description: 'Brief details about the event go here.',
+      image: '/assets/events.avif'
+    },
+    {
+      title: 'Event Title 5',
+      description: 'Brief details about the event go here.',
+      image: '/assets/4.jpg'
+    }
+  ];
 }
