6c437ef54ed59d8b12c645b597b37029f2054e32
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-18 12:30:07 +0300

    create  events angular application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {FooterComponent} from "./footer/footer.component";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink,CommonModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'events-app-new';
+
+  links = [
+    { label: 'Home', path: '/' },
+    { label: 'Events', path: '/events' },
+    { label: 'Login', path: '/login' },
+    { label: 'Signup', path: '/signup' }
+  ];
 }
