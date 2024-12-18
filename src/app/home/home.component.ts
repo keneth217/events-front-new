e979f320923db3ccd486c1aa553c938bb33ff729
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-18 20:45:59 +0300

    add content to pages,integrate login api

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,10 +10,37 @@ import {RouterLink, RouterOutlet} from "@angular/router";
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  links = [
-    { label: 'Home', path: '/' },
-    { label: 'Events', path: '/events' },
-    { label: 'Login', path: '/login' },
-    { label: 'Signup', path: '/signup' }
+  // Array of event objects
+  events = [
+    {
+      title: 'Event Title 1',
+      description: 'Brief details about the event go here.',
+      image: '/assets/3.jpg'
+    },
+    {
+      title: 'Event Title 2',
+      description: 'Brief details about the event go here.',
+      image: '/assets/2.jpg'
+    },
+    {
+      title: 'Event Title 3',
+      description: 'Brief details about the event go here.',
+      image: '/assets/events.jpg'
+    },
+    {
+      title: 'Event Title 4',
+      description: 'Brief details about the event go here.',
+      image: '/assets/events.avif'
+    },
+    {
+      title: 'Event Title 5',
+      description: 'Brief details about the event go here.',
+      image: '/assets/4.jpg'
+    }
   ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
