cb70275bb955bf219a70ba32e973bc646bb7e3c0
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-18 12:30:28 +0300

    create  events angular application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./login/login.component";
+import {SignupComponent} from "./signup/signup.component";
+import {EventsListComponent} from "./events-list/events-list.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: '', component: HomeComponent },
+
+  { path: 'events', component: EventsListComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
+];
