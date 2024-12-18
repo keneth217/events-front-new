cd0cd6f4318ee5c8d68b4d0e9000c2e55f82f133
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-18 20:45:22 +0300

    add content to pages,integrate login api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
-import {HomeComponent} from "./home/home.component";
+
 import {FooterComponent} from "./footer/footer.component";
 import {CommonModule} from "@angular/common";
-
+// @ts-ignore
+import * as AOS  from 'aos';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -17,7 +18,19 @@ export class AppComponent {
   links = [
     { label: 'Home', path: '/' },
     { label: 'Events', path: '/events' },
+    { label: 'About', path: '/about' },
     { label: 'Login', path: '/login' },
     { label: 'Signup', path: '/signup' }
   ];
+
+  isLoading: boolean=true;
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  ngOnInit(): void {
+    // @ts-ignore
+    AOS.init(); // Initialize AOS
+  }
 }
