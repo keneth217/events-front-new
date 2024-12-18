a198d5e0599bb7ef9f964b1ff2245e2b56815e3e
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-18 12:30:53 +0300

    create  events angular application

---
+++ src/app/addevent/addevent.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addevent',
+  standalone: true,
+  imports: [],
+  templateUrl: './addevent.component.html',
+  styleUrl: './addevent.component.css'
+})
+export class AddeventComponent {
+
+}
