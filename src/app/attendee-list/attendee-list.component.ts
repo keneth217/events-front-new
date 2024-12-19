b940ceebc00aab981423fc792f1580e81fdf3290
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-19 22:34:02 +0300

    integrate login api for events

--- src/app/attendee-list/attendee-list.component.ts
+++ src/app/attendee-list/attendee-list.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {CurrencyPipe} from "@angular/common";
+import {AttendantService} from "../../service/attendant.service";
 
+
+export interface TicketElement {
+  ticketId: string;
+  eventName: string;
+  eventCost: number;
+  ticketQuantity: number;
+  userName: string;
+  paidAmount: number;
+  scanned: boolean;
+
+}
 @Component({
   selector: 'app-attendee-list',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTableModule, MatPaginatorModule, CurrencyPipe
+  ],
   templateUrl: './attendee-list.component.html',
-  styleUrl: './attendee-list.component.css'
+  styleUrls: ['./attendee-list.component.css'],
 })
-export class AttendeeListComponent {
+export class AttendeeListComponent implements AfterViewInit {
+  displayedColumns: string[] = ['ticketId','userName', 'eventName', 'eventCost', 'ticketQuantity', 'Paid','Pay'];
+  dataSource = new MatTableDataSource<TicketElement>([]);
 
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+  isLoading: boolean = false;
+
+  constructor(private attendService: AttendantService) {}
+
+  ngOnInit(): void {
+    this.fetchEventsAttendants();
+  }
+
+  ngAfterViewInit() {
+    if (this.paginator) {
+      this.dataSource.paginator = this.paginator;
+    }
+  }
+
+  fetchEventsAttendants(): void {
+    this.isLoading = true;
+
+    this.attendService.getMyTickets().subscribe({
+      next: (data: TicketElement[]) => {
+        this.dataSource.data = data;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Error fetching events:', error);
+        this.isLoading = false;
+      }
+    });
+  }
 }
