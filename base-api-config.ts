1ac1159a9300fa9843d86fc228e06f80ab60d01d
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-19 22:34:37 +0300

    integrate login api for events

--- base-api-config.ts
+++ base-api-config.ts
@@ -2,5 +2,5 @@ export const API_CONFIG = {
   //dev
  // BASE_URL:'https://shop-backend-iwr1.onrender.com/api',
   //prod
-   BASE_URL:'http://localhost:8080/api'
+   BASE_URL:'http://localhost:8080/api/v1'
 };
