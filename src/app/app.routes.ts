25b1555490b4b3a0a46cbaef6b8d6ee1c7d919f2
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-19 22:34:41 +0300

    integrate login api for events

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,32 @@
 import { Routes } from '@angular/router';
+
+import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
+import { EventsListComponent } from "./events-list/events-list.component";
+import { AboutComponent } from "./about/about.component";
+import { EventDetailsComponentComponent } from "./event-details-component/event-details-component.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { AttendeeListComponent } from "./attendee-list/attendee-list.component";
 import {HomeComponent} from "./home/home.component";
-import {LoginComponent} from "./login/login.component";
-import {SignupComponent} from "./signup/signup.component";
-import {EventsListComponent} from "./events-list/events-list.component";
-import {AboutComponent} from "./about/about.component";
-import {EventDetailsComponentComponent} from "./event-details-component/event-details-component.component";
-import {DashboardComponent} from "./dashboard/dashboard.component";
+import {CategoryListComponent} from "./category-list/category-list.component";
+import {AddeventComponent} from "./addevent/addevent.component";
+import {MyticketsComponent} from "./mytickets/mytickets.component";
 
 export const routes: Routes = [
-
-  { path: '', component: HomeComponent },
+  { path: '', component   : HomeComponent }, // Fixed
   { path: 'about', component: AboutComponent },
   { path: 'events', component: EventsListComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'event-details/:title', component:EventDetailsComponentComponent},
+  { path: 'list', component: AttendeeListComponent },
+  { path: 'event-details/:eventId', component: EventDetailsComponentComponent },
+  { path: 'dash', component: DashboardComponent ,
+  children:[
+    { path: 'events', component: EventsListComponent },
+    { path: 'add', component: AddeventComponent },
 
-
-  { path: 'dash', component: DashboardComponent },
+    { path: 'my-tickets', component: MyticketsComponent },
+    { path: 'category', component: CategoryListComponent },
+  ],
+  },
 ];
