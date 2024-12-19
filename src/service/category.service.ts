fdecb78cb495611ca547199339da24e4f79227ad
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-19 22:33:25 +0300

    integrate login api for events

--- src/service/category.service.ts
+++ src/service/category.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import {API_CONFIG} from "../../base-api-config";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {TokenService} from "./token.service";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  private BASE_URL:string =API_CONFIG.BASE_URL
+
+
+
+  constructor(private http: HttpClient, private tokenService: TokenService) { }
+
+  // Method to add a product
+  addCategory(productData: FormData): Observable<any> {
+    return this.http.post<any>(
+      `${this.BASE_URL}/admin/products/add`,
+      productData,
+      { headers: this.createAuthorizationHeaders() }
+    );
+  }
+
+  // Fetch products from the API
+  getCategories(): Observable<any[]> {
+
+
+    return this.http.get<any[]>(
+      `${this.BASE_URL}/admin/category`,
+      { headers: this.createAuthorizationHeaders() }
+    );
+  }
+
+  // Create authorization headers
+  private createAuthorizationHeaders(): HttpHeaders {
+    const token = this.tokenService.getToken;
+    return new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  }
 }
